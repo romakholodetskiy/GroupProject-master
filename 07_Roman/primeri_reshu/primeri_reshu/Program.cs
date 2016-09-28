434ef50f1a8ce7e4f56b85229295a7ea0a854f90
romak <romak@DESKTOP-571RNQ1>
2016-09-28 20:48:17 +0300

    Add projects

---
+++ 07_Roman/primeri_reshu/primeri_reshu/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeri_reshu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) throw new ArgumentException("Value cannot be an empty collection.", nameof(args));
+            if (args.Length != 3) throw new ArgumentException("Value cannot be an empty collection.", nameof(args));
+            double pershe = double.Parse(args[0]);
+            double druge = double.Parse(args[2]);
+            string diya = args[1];
+            //Console.Write("Введите первое число=>");
+            //string per=Console.ReadLine();
+            //Console.Write("ВВедите второе число=>");
+            //string dr = Console.ReadLine();
+            //double pershe = double.Parse(per);
+            //double druge=double.Parse(dr);
+            double res=0;
+            //Console.Write("Введите операцию=>");
+            //string diya = Console.ReadLine();
+            if (diya == "+") res = pershe + druge;
+            if (diya == "-") res = pershe - druge;
+            if (diya == "*") res = pershe*druge;
+            if (diya == "/") res = pershe/druge;
+            Console.WriteLine("Результат=>"+res);
+            Console.ReadKey();
+        }
+    }
+}
