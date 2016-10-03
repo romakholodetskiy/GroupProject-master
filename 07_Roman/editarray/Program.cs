cbb3179eefb061ec57ad26d1b46a833b557a24db
romak <romak@DESKTOP-571RNQ1>
2016-10-03 19:53:23 +0300

    edit

--- 07_Roman/editarray/Program.cs
+++ 07_Roman/editarray/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace editarray
@@ -19,6 +20,7 @@ namespace editarray
                 for (int j = 0; j < array[i].Length; j++)
                 {
                     array[i][j] = new Random().Next(0, 9);
+                    Thread.Sleep(100);
                 }
             }
             for (int i = 0; i < array.Length; i++)
@@ -27,6 +29,7 @@ namespace editarray
                 {
                     Console.Write(array[i][j]);
                 }
+                Console.WriteLine();
             }
             Console.ReadKey();
         }
